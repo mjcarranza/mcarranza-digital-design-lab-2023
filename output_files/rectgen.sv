373e0a645241bd9ea13ba324ff623290618d67d7
Mario <48070830+mjcarranza@users.noreply.github.com>
2023-10-14 21:20:23 -0600

    Se mustran cuadorados en la panatalla

--- output_files/rectgen.sv
+++ output_files/rectgen.sv
@@ -1,8 +1,25 @@
 
-module rectgen(
-	input logic [9:0] x, y, left, top, right, bot,
-	output logic inrect
+// con este modulo genero un rectangulo de dimensiones width x height
+
+module rectgen #(parameter WIDTH = 76, HEIGHT = 56) (
+	input logic [9:0] x, y, // paso las posiciones de x e y en la pantalla
+	output logic [WIDTH-1:0] out, // La salida es un vector de longitud WIDTH-1
+	output logic [7:0] color	// salida del color asociado al vector generado
 	);
+						//				left		top		right		bot
+	// rectgen rectgen(x, y, 10'd120, 10'd150, 10'd200, 10'd230, inrect);	
+	//assign inrect = (x >= left & x < right & y >= top & y < bot);
 	
-	assign inrect = (x >= left & x < right & y >= top & y < bot);
+	// 
+	always_comb begin
+		 if (x < WIDTH && y < HEIGHT) begin
+			  out = 1'b1;
+			  color = 8'hFF; // Gris (128 en decimal)
+		 end
+		 else begin
+			  out = 1'b0;
+			  color = 8'h00; // Sin color (negro)
+		 end
+	end
+
 endmodule 
