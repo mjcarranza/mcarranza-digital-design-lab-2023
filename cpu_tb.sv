ea434ced25da42490f0d70e41603134622ad9029
Mario <48070830+mjcarranza@users.noreply.github.com>
2023-11-22 04:34:03 -0600

    Update cpu_tb.sv

--- cpu_tb.sv
+++ cpu_tb.sv
@@ -1,76 +1,50 @@
+`timescale 1ns/1ps
 
-// Test bench para el módulo del CPU
-module cpu_tb();
+module cpu_tb;
 
-    // Entradas
-    reg clkFPGA;
-    reg rst;
+   // Inputs
+   logic clkFPGA;
+   logic rst;
 
-    // Salidas
-    reg [31:0] pc, pc_out, Inst, RD1, RD2, ExtImm, SrcA, SrcB, ALUResult, ReadData, Result, PCPlus1, PCPlus2;
-    reg PCSrc, MemtoReg, MemWrite, RegWrite, cout, negative, zero, ALUSrc;
-    reg [1:0] ImmSrc, RegSrc, ALUControl;
-    reg [3:0] RA1, RA2;
+   // Outputs
+   logic vgaclk, hsync, vsync, sync_b, blank_b;
+   logic [7:0] r, g, b;
 
-    // Conectar el módulo principal
-    cpu cpu_inst (
-        .clkFPGA(clkFPGA), 
-        .rst(rst),
-        .pc(pc),
-        .pc_out(pc_out),
-        .Inst(Inst),
-        .RD1(RD1),
-        .RD2(RD2),
-        .ExtImm(ExtImm),
-        .SrcA(SrcA),
-        .SrcB(SrcB),
-        .ALUResult(ALUResult),
-        .ReadData(ReadData),
-        .Result(Result),
-        .PCPlus1(PCPlus1),
-        .PCPlus2(PCPlus2),
-        .PCSrc(PCSrc),
-        .MemtoReg(MemtoReg),
-        .MemWrite(MemWrite),
-        .RegWrite(RegWrite),
-        .cout(cout),
-        .negative(negative),
-        .zero(zero),
-        .ALUSrc(ALUSrc),
-        .ImmSrc(ImmSrc),
-        .RegSrc(RegSrc),
-        .ALUControl(ALUControl),
-        .RA1(RA1),
-        .RA2(RA2)
-    );
+   // Instantiate the CPU module
+   cpu cpu_inst(
+      .clkFPGA(clkFPGA),
+      .rst(rst),
+      .vgaclk(vgaclk),
+      .hsync(hsync),
+      .vsync(vsync),
+      .sync_b(sync_b),
+      .blank_b(blank_b),
+      .r(r),
+      .g(g),
+      .b(b)
+   );
 
-    // Generar un clock de prueba
-    initial begin
-        clkFPGA = 0;
-        forever #5 clkFPGA = ~clkFPGA;
-    end
+   // Clock generation
+   initial begin
+      clkFPGA = 0;
+      forever #5 clkFPGA = ~clkFPGA;
+   end
 
-    // Inicializar las entradas
-    initial begin
-        rst = 1;
-        #10 rst = 0;
+   // Reset generation
+   initial begin
+      rst = 1;
+      #10 rst = 0;
+      //#1000 $finish;
+   end
 
-        // Inicializar la instrucción en la ROM como una instrucción ADD
-        // Aquí asumimos que la instrucción está codificada como add rd, rs1, rs2
-        pc = 0;
-        {Inst[6:0], Inst[14:12], Inst[24:20]} = 7'b0000000; // Opcode: ADD
-        {Inst[11:7], Inst[19:15], Inst[31:25]} = {5'd1, 5'd2, 5'd3}; // rd, rs1, rs2
+   // Monitor to display VGA signals
+   always @(posedge vgaclk) begin
+      $display("Time: %0t, HSYNC: %b, VSYNC: %b, BLANK_B: %b", $time, hsync, vsync, blank_b);
+   end
 
-        // Esperar un tiempo suficiente para la ejecución
-        #100;
-
-        // Verificar la salida esperada para la instrucción ADD
-        if (Result !== RD1 + RD2) begin
-            $display("Error: Resultado incorrecto para la instrucción ADD");
-        end else begin
-            $display("Instrucción ADD ejecutada correctamente");
-        end
-
-    end
+   // Stimulus generation (you can add more meaningful stimuli)
+   initial begin
+      #20; 
+   end
 
 endmodule
